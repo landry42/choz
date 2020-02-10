dc8d9f59c8928d3fc0efe1437c41e9211335b8cf
Landry VALETOUX <landry.valetoux@etu.univ-orleans.fr>
2020-02-10 12:00:25 +0100

    v2

--- choz.js
+++ choz.js
@@ -1,17 +1,43 @@
 choz = {};
 
 choz.init = function(){
-    jsPanel.create({
-        theme:       'primary',
-        headerTitle: 'my panel #1',
-        position:    'center-top 0 58',
-        contentSize: '450 250',
-        content:     '<p>Example panel ...</p>',
-        callback: function () {
-            this.content.style.padding = '20px';
-        },
-        onbeforeclose: function () {
-            return confirm('Do you really want to close the panel?');
-        }
-    });
+    choz.terminal.open();
 }
+
+choz.terminal = {
+    open: function(){
+        jsPanel.create({
+            headerTitle: 'Terminal',
+            content:
+            '<div id="terminal">'+
+            '<textarea name="" id="terminal-out" cols="30" rows="10" readonly></textarea>'+
+            '<input id="terminal-in" type="text" onchange="choz.terminal.keyWord(this.value)">'+
+            '</div>',
+        });
+    },
+    keyWord: function(text){
+        text = text.split(" ");
+        res = [];
+        text.forEach(element => {
+            if (choz.command.includes(element)){
+                res.push(element);
+            }
+        });
+        choz.terminal.enter(res);
+    },
+    enter: function(command){
+        if(command[0] === 'echo'){
+            if(command.size > 1){
+                choz.terminal.write(command[1]); 
+            }
+        }
+    },
+    write: function(text){
+        document.getElementById("terminal-out").value += text+"\n";
+        document.getElementById("terminal-in").value = "";
+    }
+}
+
+choz.command = [
+    'echo',
+]
