f8ec57099952aeef105992f31cd11bf953b5a67c
Landry VALETOUX <landry.valetoux@etu.univ-orleans.fr>
2020-02-11 16:32:11 +0100

    maj 11/02/20 16:32

--- public/main.js
+++ public/main.js
@@ -1,100 +1,82 @@
-mindfork = {
-
-  init:function(){
-    this.open(this.panel["terminal"]);
-    Print("Bienvenu(e) sur le terminal");
-  },
-
-  open:function(panel){
-    panel.theme = this.theme;
-    panel.iconfont = 'material-icons';
-    panel.borderRadius = 5;
-    jsPanel.create(panel);
+forkos = {
+  boot:function(){
+    this.bin.panel.run("terminal");
+    write("Bienvenu(e) sur le terminal");
   },
 
   theme: {
-    bgPanel: '#404040',
-    bgContent: '#202020',
-    colorHeader: 'white',
-    border: 'none'
-  },
-
-  terminal:{
-    keyWord: function(text){
-        res = text.split(" ");
-        mindfork.terminal.enter(res);
-    },
-    enter: function(command){
-      Print("");
-      this.commandList = {
-        echo:function(){
-          Print(command.slice(1,command.length).join(" "));
-        },
-        help:function(){
-          Print("Voici les commandes disponibles:");
-          for(elem in this){
-            Print("- "+elem);
-          }
-        },
-        panel:function(){
-          if(command.length > 1){
-            mindfork.open(mindfork.panel[command[1]]);
-          }else{
-            Print("Il manque le nom du Panel, il doit appartenir à cette liste");
-            for(elem in mindfork.panel){
-              Print("- "+elem);
-            }
-          }
-        },
-      }
-      if (command[0] in mindfork.panel){
-        mindfork.open(mindfork.panel[command[0]]);
-      } else if (command[0] in this.commandList){
-        this.commandList[command[0]]();
-      } else {
-        this.write("Commande non reconnue, tapez 'help' pour plus d'infos","error");
-      }
-      document.getElementById("terminal-in").value = "";
-    },
-    resize: function () {
-        var ta =  document.getElementById("terminal-out")
-        ta.style.cssText = 'height:0px';
-        ta.style.cssText = 'height:' + ta.scrollHeight + 'px';
-        ta.scrollTop = ta.scrollHeight;
-    },
-    write: function(text, type = "log"){
-        document.getElementById("terminal-out").value += text+"\n";
-        
-        this.resize();
+    default:{
+      bgPanel: '#404040',
+      bgContent: '#202020',
+      colorHeader: 'white',
+      border: 'none'
     }
   },
-  panel:{
+  bin:{
+    help:{
+      run:function(){
+        write("Voici les commandes disponibles");
+        for (elem in forkos.bin){
+          write(" - "+elem);
+        }
+      }
+    },
+    echo:{
+      run:function(){
+        write("argument manquant");
+      },
+      run:function(text){
+        write(text)
+      }
+    },
     terminal:{
-      headerTitle: 'Terminal',
-      content:
-      '<dic id="terminal">'+
-      '<textarea name="" id="terminal-out"  readonly></textarea>'+
-      '<input id="terminal-in" type="text" onchange="mindfork.terminal.keyWord(this.value)">'+
-      '</div>',
+      run:function(){
+        forkos.bin.panel.run("terminal");
+      },
+      keyWord: function(text){
+          res = text.split(" ");
+          forkos.bin.terminal.enter(res);
+      },
+      enter: function(command){
+        write("");
+        if (command[0] in forkos.bin){
+          forkos.bin[command[0]].run(command);
+        } else {
+          this.write("Commande non reconnue, tapez 'help' pour plus d'infos","error");
+        }
+        document.getElementById("terminal-in").value = "";
+      },
+      resize: function () {
+          var ta =  document.getElementById("terminal-out")
+          ta.style.cssText = 'height:0px';
+          ta.style.cssText = 'height:' + ta.scrollHeight + 'px';
+          ta.scrollTop = ta.scrollHeight;
+      },
+      write: function(text, type = "log"){
+          document.getElementById("terminal-out").value += text+"\n";
+          this.resize();
+      }
     },
-    button:{
-      headerTitle: 'Button',
-      content:
-      '<div class="center">'+
-      '<button onclick = "alert(\'Et il fonctionne!\')">Et voici un bouton</button>'+
-      '</div>',
-    },
-    twobutton:{
-      headerTitle: '2 Button',
-      content:
-      '<div class="center">'+
-      '<button onclick = "alert(\'Et il fonctionne!\')">Et voici un bouton</button>'+
-      '<button onclick = "alert(\'Et il fonctionne!\')">Et voici un bouton</button>'+
-      '</div>',
+    panel:{
+      run: function(panel){
+        panel = this[panel]
+        panel.theme = forkos.theme.default;
+        panel.iconfont = 'material-icons';
+        panel.borderRadius = 5;
+        jsPanel.create(panel);
+      },
+      terminal:{
+        headerTitle: 'TERMINAL',
+        content:
+        '<dic id="terminal">'+
+        '<textarea name="" id="terminal-out"  readonly></textarea>'+
+        '<input id="terminal-in" type="text" onchange="forkos.bin.terminal.keyWord(this.value)">'+
+        '</div>',
+      },
     }
-  }
+  },
 }
 
-Print = function(text){
-  mindfork.terminal.write(text);
+write = function(text){
+  forkos.bin.terminal.write(text);
 }
